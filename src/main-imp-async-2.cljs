0acfd46a2ca2ece7fb07b8c6515614736ba1b17c
Samuel Tschiedel <mukaobr@gmail.com>
2022-10-08 13:08:06 -0300

    feat: add example with print moved alongside require

---
+++ src/main-imp-async-2.cljs
@@ -0,0 +1,8 @@
+;; Fails: Imperative delayed async import
+(ns main
+  (:require [promesa.core :as p]))
+
+;; It tries to load helper.mjs from '/src/helper.mjs'
+;;                       instead of '/src/modules/module/helper.mjs'
+(p/do (require '[modules.module.core])
+      (println "Loaded main.cljs"))
